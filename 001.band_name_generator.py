ad20a7e3e2235537607957e638ccfc1e75de7b39
DariaRutkovskaia <rutkovskaya.darya.a@gmail.com>
2022-12-11 17:47:03 +0200

    Generator created

--- 001.band_name_generator.py
+++ 001.band_name_generator.py
@@ -1,1 +1,4 @@
-
+print("Welcome to the Band Name Generator!")
+city = input("Which city did you grow in?\n")
+name = input("What's name of your pet?\n")
+print(f"The name of your band should be: {city} {name}")
