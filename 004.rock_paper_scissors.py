4ed70ea77090dc6e48ab2e04c9b20f52c41e33ee
DariaRutkovskaia <rutkovskaya.darya.a@gmail.com>
2022-12-11 20:31:00 +0200

    Game created

--- 004.rock_paper_scissors.py
+++ 004.rock_paper_scissors.py
@@ -1,1 +1,46 @@
+import random
 
+rock = '''
+rock
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+paper
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+scissors
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game = [rock, paper, scissors]
+computer_num = random.randint(0, 2)
+player_num = int(input("What do you choose?\n Rock - 1\n Paper - 2\n Scissors - 3\n")) - 1
+
+computer_choice = game[computer_num]
+players_choice = game[player_num]
+print(f"Player's choice:\n {players_choice}")
+print(f"Computer's choice:\n {computer_choice}")
+if computer_num == player_num:
+    print("It's a draw")
+elif player_num - computer_num == 1 or player_num - computer_num == -2:
+    print("Player wins!!")
+else:
+    print("Computer wins!")
