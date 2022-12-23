e82c86204f49103233509f7b5fd821ef5408cd43
DariaRutkovskaia <rutkovskaya.darya.a@gmail.com>
2022-12-23 17:05:35 +0200

    Game created

--- 011.blackjack/011.blackjack.py
+++ 011.blackjack/011.blackjack.py
@@ -7,7 +7,7 @@ def deal_cards(players_cards):
 
 
 def calculate_score(players_cards):
-    if len(players_cards) == 2 and 10 in players_cards and 11 in players_cards:
+    if len(players_cards) == 2 and sum(players_cards) == 21:
         return 0
     elif 11 in players_cards and sum(players_cards) > 21:
         players_cards.remove(11)
@@ -39,26 +39,21 @@ def blackjack():
     for i in range(2):
         deal_cards(users_cards)
         deal_cards(computers_cards)
-    comp_first_card = computers_cards[0]
     users_score = calculate_score(users_cards)
     comp_score = calculate_score(computers_cards)
     print(f"Your cards: {users_cards}, current score: {users_score}")
-    print(f"Computer's first card: {comp_first_card}")
+    print(f"Computer's first card: {computers_cards[0]}")
     print('\n' * 2)
-    if calculate_score(users_cards) == 0 or calculate_score(computers_cards) == 0:
-        compare(users_score, comp_score)
-    elif calculate_score(users_cards) > 21:
+    if users_score == 0 or comp_score == 0 or users_score > 21:
         compare(users_score, comp_score)
     else:
         while continue_game and input('Type "y" to get another card. Type "n" to pass ') == "y":
             deal_cards(users_cards)
             users_score = calculate_score(users_cards)
             print(f"Your cards: {users_cards}, current score: {users_score}")
-            print(f"Computer's first card: {comp_first_card}")
+            print(f"Computer's first card: {computers_cards[0]}")
             print('\n' * 2)
-            if calculate_score(users_cards) == 0 or calculate_score(computers_cards) == 0:
-                continue_game = False
-            elif calculate_score(users_cards) > 21:
+            if calculate_score(users_cards) > 21:
                 continue_game = False
 
         else:
@@ -69,12 +64,10 @@ def blackjack():
         print(f"Your final hand: {users_cards}, final score: {users_score}")
         print(f"Computer's final hand: {computers_cards}, final score: {comp_score}")
     print('\n' * 2)
-    if input('Do you want to play a game of BlackJack? Type "y" or "n" ') == "y":
-        blackjack()
 
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-if input('Do you want to play a game of BlackJack? Type "y" or "n" ') == "y":
+while input('Do you want to play a game of BlackJack? Type "y" or "n" ') == "y":
     from art import logo
 
     print(logo)
