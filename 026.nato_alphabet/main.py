9f90c1f76d4bbac83ba662a6a4b06744e75e708c
DariaRutkovskaia <rutkovskaya.darya.a@gmail.com>
2023-02-17 17:30:15 +0200

    NATO alphabet updated

--- 026.nato_alphabet/main.py
+++ 026.nato_alphabet/main.py
@@ -1,6 +1,12 @@
 import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 code_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-users_word = input("Please, enter your word: ").upper()
-code_list = [f"{letter} for {code_dict[letter]}" for letter in users_word if letter != " "]
-print(code_list)
+while True:
+    users_word = input("Please, enter your word: ").upper()
+    try:
+        code_list = [f"{letter} for {code_dict[letter]}" for letter in users_word if letter != " "]
+    except KeyError:
+        print("Only letters in the alphabet, please")
+    else:
+        print(code_list)
+        break
